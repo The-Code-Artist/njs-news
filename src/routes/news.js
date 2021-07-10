9da6b390a6beea9f98829cbac32791d5c8800758
The Code Artist <code_artist@outlook.com>
2021-07-10 10:28:29 +0200

    Styling, Homepage updates and API calls to retrieve articles.

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const axios = require("axios");
+
 const newsRouter = express.Router();
 
 newsRouter.get('', async(req, res) => {
-    res.render('news');
+    try {
+        const endpoint = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts`);
+        res.render('news', { articles: endpoint.data });
+    } catch (err) {
+        if (err.response) {
+            console.log(err.response.data);
+            console.log(err.response.status);
+        } else if (err.request)
+            console.log(err.request);
+        else
+            console.log("Error", err.message);
+    }
 });
 
  module.exports = newsRouter;
